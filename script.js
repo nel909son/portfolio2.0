4668808cac8aeb69ac9500e84c07cff5113b3367
Nelson <nelsonoffley@gmail.com>
2021-11-08 09:17:41 -0800

    maybe final commit

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function darkMode() {
     let div1 = document.querySelector(".div1");
     let div2 = document.querySelector(".div2");
     let scrollBar = document.querySelector(".scroll-bar");
+    let aTags = document.querySelectorAll("a")
     body.classList.toggle("body-dark");
     div1.classList.toggle("div1-dark");
     div2.classList.toggle("div2-dark");
     scrollBar.classList.toggle("scroll-bar-dark");
+    aTags.forEach(tag => tag.classList.toggle("a-dark"))
+    
  }
